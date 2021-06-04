6a732c3b9c03c0e6a0b159c3855f546e9dead1dd
megamagomed <34594010+megamagomed@users.noreply.github.com>
2021-06-04 11:56:37 +0300

    Add files via upload

---
+++ lesson300521/script.js
@@ -0,0 +1,83 @@
+// function buttonClick() {
+//     console.log('Hello world!')
+// }
+
+let button = document.querySelector('#button');
+button.addEventListener('click', buttonClick)
+
+let navbar = document.querySelector('#navbar span');
+function buttonClick() {
+    navbar.textContent = 'Did you push the button?'
+}
+
+
+let output = document.querySelector('#output');
+let box = document.querySelector('#box');
+box.addEventListener('mousemove', runEvent);
+
+function runEvent(event) {
+    output.innerHTML = '<h3>MouseX ' + event.offsetX + '</h3>' + '<h3>MouseY ' + event.offsetY + '</h3>';
+}
+
+function runEvent(event) {
+    box.style.backgroundColor = 'rgb(' + event.offsetX + ',' + event.offsetY + ', 40)'
+}
+
+let input = document.querySelector('input[type="text"]');
+let form = document.querySelector('form');
+
+input.addEventListener('keydown', runEvent);
+function runEvent(event) {
+    output.innerHTML = '<h4>' + event.target.value + '</h4>'
+}
+
+
+
+
+
+
+
+box = {
+    props: { offsetX: 10, offsetY: 20 },
+
+    addEventListener: function (event, func) {
+        if (event = 'mousemove') {
+            func(this.props)
+        }
+    }
+}
+
+box.addEventListener('mousemove', runEvent)
+
+function runEvent(event) {
+    console.log(event.offsetX);
+    console.log(event.offsetY);
+}
+
+
+
+
+let items = document.querySelector('#items');
+form.addEventListener('submit', addItem);
+
+function addItem(event) {
+    event.preventDefault();
+    let input = document.querySelector('input[type="text"]').value;
+
+    let li = document.createElement('li');
+    li.appendChild(document.createTextNode(input));
+    li.className = 'list-group-item';
+
+    let deleteButton = document.createElement('button');
+    deleteButton.className = 'btn btn-danger btn-sm float-right';
+    deleteButton.appendChild(document.createTextNode('X'));
+    li.appendChild(deleteButton);
+
+    items.appendChild(li);
+}
+
+items.addEventListener('click', deleteItem);
+function deleteItem(event) {
+    let li = event.target.parentElement;
+    items.removeChild(li);
+}
